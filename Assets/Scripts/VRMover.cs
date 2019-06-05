270e9416bc0e36864800bcb97dfd45291006b28b
Kirill Kovalevskiy <kovalewskiy@gmail.com>
2019-06-05 23:43:38 +0900

    Added OVR and configured. Added simple controller script

---
+++ Assets/Scripts/VRMover.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRMover : MonoBehaviour
+{
+  public float speed = 1.0f;
+  private Transform cam;
+
+  void Start()
+  {
+    // This camera is controlled by headset rotation
+    cam = transform.Find("Camera").GetComponent<Transform>();
+  }
+
+  void Update()
+  {
+    // Quaternion q = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTrackedRemote);
+    // bool isConnected = OVRInput.IsControllerConnected(OVRInput.Controller.RTrackedRemote);
+
+    if (OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger))
+    {
+      transform.Translate(cam.forward * speed * Time.deltaTime, Space.World);
+    }
+
+    if (OVRInput.Get(OVRInput.Button.PrimaryTouchpad))
+    {
+      transform.Translate(-cam.forward * speed * Time.deltaTime, Space.World);
+    }
+  }
+}
