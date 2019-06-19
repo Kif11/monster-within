98d03900512d632d9c644bade4a9750dbf60affb
Sneha Belkhale <sbelkhale@yahoo.com>
2019-06-19 17:25:19 +0900

    resizing scene, adding lamp with sound effects

---
+++ Assets/Scripts/MetalCollisionDetector.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MetalCollisionDetector : MonoBehaviour
+{
+    AudioSource audioSource;
+    void Start() {
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision with metal");
+        audioSource.Play();
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
